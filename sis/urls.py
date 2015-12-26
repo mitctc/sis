f7fe9e5607ecf4aa2b4486d933e7ba621e4c9ffa
Don Sumudu Saranga Hettiarachchi <sumudu.susahe@gmail.com>
2015-12-26 17:10:12 +0530

    change settings file and add new url system

--- sis/urls.py
+++ sis/urls.py
@@ -14,9 +14,20 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url
+from django.conf.urls import url,patterns
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
+
+
+
+
+urlpatterns = [
+    # ... the rest of your URLconf goes here ...
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
