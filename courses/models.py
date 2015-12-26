d74daca84d6753b28ef27d59a397932e0e6617fd
Don Sumudu Saranga Hettiarachchi <sumudu.susahe@gmail.com>
2015-12-26 17:41:13 +0530

    Create a Model Course and Topic

--- courses/models.py
+++ courses/models.py
@@ -4,4 +4,18 @@ from django.db import models
 
 # Create your models here.
 
+class Course(models.Model):
+	code = models.CharField(max_length=5,unique=True)
+	name = models.CharField(max_length=128)
+	views = models.IntegerField(default=0)
 	
+	def __unicode(self):
+		return self.name 
+		
+class Topic(models.Model):
+	course = models.ForeignKey(Course)
+	code = models.CharField(max_length=5,unique=True)
+	name = models.CharField(max_length=128)
+	
+	def __unicode(self):
+		return self.name 
--- sis/urls.py
+++ sis/urls.py
@@ -21,13 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-
-
-
-urlpatterns = [
-    # ... the rest of your URLconf goes here ...
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
