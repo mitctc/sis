d656051eeeb05ccc9842b157bd0e4c03504a690b
Don Sumudu Saranga Hettiarachchi <sumudu.susahe@gmail.com>
2015-12-26 20:01:24 +0530

    Add Views and HTML Documents

--- sis/urls.py
+++ sis/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url,patterns
+from django.conf.urls import url,patterns,include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-
+#from courses import urls 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+	url(r'^course/',include('courses.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
