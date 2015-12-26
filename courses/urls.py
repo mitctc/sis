2660ba08d53d17380a18f19c3d304ac658dbd87a
Don Sumudu Saranga Hettiarachchi <sumudu.susahe@gmail.com>
2015-12-26 20:33:09 +0530

    Add forms to projects

--- courses/urls.py
+++ courses/urls.py
@@ -2,4 +2,4 @@ from django.conf.urls import patterns, url
 from courses import views
 ##
 urlpatterns = patterns('', 
-				url(r'^$', 'views.index', name='index'),) # NEW MAPPING!
+				url(r'^$', views.index, name='index'),) # NEW MAPPING!
--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from courses.forms import CourseForm
 
 # Create your views here.
 def index(request):
@@ -10,4 +11,28 @@ def index(request):
     context_dict = {'categories': "course list"}
 
     # Render the response and send it back!
-    return render(request, 'courses/index.html', context_dict)
+    return render(request, 'courses/index.html', context_dict
+
+def add_course(request):
+    # A HTTP POST?
+    if request.method == 'POST':
+		form = CourseForm(request.POST)
+
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+
+            # Now call the index() view.
+            # The user will be shown the homepage.
+            return index(request)
+        else:
+            # The supplied form contained errors - just print them to the terminal.
+            print form.errors
+    else:
+        # If the request was not a POST, display the form to enter details.
+        form = CourseForm()
+
+    # Bad form (or form details), no form supplied...
+    # Render the form with error messages (if any).
+    return render(request, 'courses/add_course.html', {'form': form})
