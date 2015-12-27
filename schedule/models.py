5307f92e8744cb85c0ab60836517a6dd5f3436ed
Don Sumudu Saranga Hettiarachchi <sumudu.susahe@gmail.com>
2015-12-27 14:09:13 +0530

    create model in side schedule and create database table

--- schedule/models.py
+++ schedule/models.py
@@ -1,2 +1,19 @@
+from __future__ import unicode_literals
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 from django.db import models
+from courses.models import CourseGroup
 
+class TheorySession(models.Model):
+	name = models.CharField(max_length=120)
+	course_group = models.ForeignKey(CourseGroup)
+	
+
+class LabSession(models.Model):
+	
+	course_group = models.ForeignKey(CourseGroup)
+
+class PracticalSession(models.Model):
+	
+	user = models.ForeignKey(User)
+	course_group = models.ForeignKey(CourseGroup)
