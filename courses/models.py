c8323943a93b9a8d50b2336c90e04386c4dd0eb4
Don Sumudu Saranga Hettiarachchi <sumudu.susahe@gmail.com>
2015-12-27 10:24:24 +0530

    Model has changed and update

--- courses/models.py
+++ courses/models.py
@@ -1,16 +1,27 @@
 from __future__ import unicode_literals
+from django.template.defaultfilters import slugify
 
 from django.db import models
 
 # Create your models here.
 
 class Course(models.Model):
-	code = models.CharField(max_length=5,unique=True)
+	code = models.CharField(max_length=10,unique=True)
 	name = models.CharField(max_length=128)
 	views = models.IntegerField(default=0)
+	likes = models.IntegerField(default=0)
+	slug = models.SlugField()
 	
-	def __unicode(self):
-		return self.name 
+	def save(self, *args, **kwargs):
+            # Uncomment if you don't want the slug to change every time the name changes
+            #if self.id is None:
+                    #self.slug = slugify(self.name)
+			self.slug = slugify(self.name)
+			super(Course, self).save(*args, **kwargs)
+
+	def __unicode__(self):
+		return self.name
+
 		
 class Topic(models.Model):
 	course = models.ForeignKey(Course)
